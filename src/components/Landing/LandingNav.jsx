83899b419f89e1152e5c9440084a8d83d78e0a15
hagar3bdelsalam <149162265+hagar3bdelsalam@users.noreply.github.com>
2025-09-02 07:09:05 +0300

    feat: make LandingNav flexible and integrate it into SelectSection component

--- src/components/Landing/LandingNav.jsx
+++ src/components/Landing/LandingNav.jsx
@@ -1,19 +1,11 @@
-export default function LandingNav() {
+export default function LandingNav({children}) {
   return (
     <nav className="w-full flex items-center justify-between p-2 bg-white shadow">
-      <div className="flex items-center space-x-2">
+      <div className="flex items-center space-x-2 ml-3">
         <img src="/Logo.png" alt="Logo" className="w-10 h-10 object-contain" />
         <span className="text-lg font-bold">Her Haven</span>
       </div>
-
-      <div className="space-x-4">
-        <button className="bg-pink-550 text-white py-2 px-4 rounded-[10px]">
-          Login
-        </button>
-        <button className="bg-pink-550 text-white py-2 px-4 rounded-[10px]">
-          Get Started
-        </button>
-      </div>
+      {children}
     </nav>
   );
 }
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,7 +4,16 @@ import LandingNav from '../components/Landing/LandingNav';
 export default function Homepage() {
   return (
     <>
-      <LandingNav />
+      <LandingNav>
+        <div className="space-x-4">
+          <button className="bg-pink-550 text-white py-2 px-4 rounded-[10px]">
+            Login
+          </button>
+          <button className="bg-pink-550 text-white py-2 px-4 rounded-[10px]">
+            Get Started
+          </button>
+        </div>
+      </LandingNav>
       <HeroImage />
     </>
   );
