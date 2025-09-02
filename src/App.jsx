d89ead0a5c39bfc96ac8e37d65fa3eeced0aba33
hagar3bdelsalam <149162265+hagar3bdelsalam@users.noreply.github.com>
2025-09-02 08:09:20 +0300

    feat: add Footer and setup routing for Get Started button

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
+import Homepage from './pages/Homepage';
+import SelectSection from './pages/SelectSection';
 
-function App() {
-  return (
-    <Router>
+  function App() {
+    return (
       <Routes>
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
-  );
+      <Route index element={<Homepage />} />
+      <Route path='select' element={<SelectSection />} />
+      <Route path="signup" element={<SignUp />} />
+      <Route path="login" element={<Login />} />
+    </Routes>
+  )
 }
 
 export default App;
---
+++ src/components/Landing/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Footer() {
+  return (
+    <div className="text-[#8A617D] py-4">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2025 Her Haven. All rights reserved.</p>
+      </div>
+    </div>
+  );
+}
