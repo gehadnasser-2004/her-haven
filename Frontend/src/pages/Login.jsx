c73cf095f57fe77829b8a84b1cfe531e64ce152b
hagar3bdelsalam <149162265+hagar3bdelsalam@users.noreply.github.com>
2025-09-03 04:41:36 +0300

    chore: move project files into Frontend/ folder

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -3,7 +3,7 @@ import LoginForm from "../components/LoginForm";
 
 export default function Login() {
   return (
-    <div class="w-screen bg-blue-500">
+    <div className="w-screen bg-blue-500">
       <div className="min-h-screen bg-gray-50">
         <Navbar buttonText="Sign up" />
         <div className="flex justify-center items-center py-12">
