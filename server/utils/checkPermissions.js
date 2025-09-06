27210473a0d77db6830afcb86fca0c7cd7859674
Doha-Ahmed-E <dohaamed22@gmail.com>
2025-09-06 17:35:13 +0300

    feat: adding back-end

---
+++ server/utils/checkPermissions.js
@@ -0,0 +1,9 @@
+import { errors } from "../errors/index.js";
+
+const checkPermissions = (requestUser, resourceUserId) => {
+  if (requestUser.role === "admin") return;
+  if (requestUser.userId === resourceUserId.toString()) return;
+  throw new errors.UnauthorizedError("Not authorized to access this route");
+};
+
+export default checkPermissions;
